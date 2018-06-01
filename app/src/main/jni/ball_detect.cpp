09a4d544338e940fb330956d7dbc0e1ae781aa84
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-06-01 15:27:36 +1000

    Got it to build, but not checked yet.  Just about to remove the "model{ " section in the build.gradle and try pushing the sections into " android{ "

--- app/src/main/jni/ball_detect.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by bobsang on 2/17/2016.
-//
-
-#include "ball_detect.h"
--- app/src/main/jni/ball_detect.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by bobsang on 2/17/2016.
-//
-
-#ifndef MYROBOT_BALL_DETECT_H
-#define MYROBOT_BALL_DETECT_H
-
-#endif //MYROBOT_BALL_DETECT_H
